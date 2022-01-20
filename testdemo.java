0ac9b0509fb74cb65916608a8555d7a92f920cbf
B V <manasa.b-v@capgemini.com>
2022-01-20 20:00:29 +0530

    first message

---
+++ testdemo.java
@@ -0,0 +1,11 @@
+public class testdemo
+{
+public static void main(String[] args)
+{
+int a=9,b=8;
+if(a>b)
+System.out.println("a is less than b");
+else
+System.out.println("b is less than a");
+}
+}
